882309fe31091e54aa2c20c18f6487a190a6a522
Tamsyn Cunningham <tamsynlckime@gmail.com>
2014-11-21 22:36:01 -0800

    add test file

---
+++ test.py
@@ -0,0 +1,21 @@
+print "Halt!"
+name = input("Who goes there?")
+print "You may pass" , name
+print ""
+question = input ("What knowledge do you desire?") 
+print "That is a good question" , name
+print question
+print "..."
+print "You must choose a guide for your answer"
+Zelda = "Your question needs careful thought"
+Soren = "I cannot tell you, the answer is dangerous"
+Dwight = "the answer is 42"
+print "Zelda"
+print "Soren"
+print "Dwight"
+guide = input ("Who is your choice?") 
+print name , guide
+print ""
+print ""
+print "Did that help?"
+
