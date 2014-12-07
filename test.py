52cd9b3a2da3a3d93953510342b2591f04573b23
Tamsyn Cunningham <tamsynlckime@gmail.com>
2014-12-06 16:50:14 -0800

    fixed input information

--- test.py
+++ test.py
@@ -1,20 +1,22 @@
-print "Halt!"
-name = input("Who goes there?")
-print "You may pass" , name
+print u"Halt!"
+name = raw_input(u"Who goes there?   ")
+print u"You may pass" , name
 print ""
-question = input ("What knowledge do you desire?") 
-print "That is a good question" , name
-print question
-print "..."
-print "You must choose a guide for your answer"
-Zelda = "Your question needs careful thought"
-Soren = "I cannot tell you, the answer is dangerous"
-Dwight = "the answer is 42"
-print "Zelda"
-print "Soren"
-print "Dwight"
-guide = input ("Who is your choice?") 
-print name , guide
+question = raw_input (u"What knowledge do you desire?   ") 
+print u"That is a good question" , name
+print question ,"?"
+print u"..."
+print u"You must choose a guide for your answer"
+Zelda = u"Your question needs careful thought"
+Soren = u"I cannot tell you, the answer is dangerous"
+Dwight = u"the answer is 42"
+Ent = u"I am Groot"
+print u"Zelda"
+print u"Soren"
+print u"Dwight"
+print u"Ent"
+guide = input ("Who is your choice?   ") 
+print name , "," , guide
 print ""
 print ""
 print "Did that help?"
